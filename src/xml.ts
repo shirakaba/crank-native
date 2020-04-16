e796cc2c8a05ddcf8bcee1ffb54e60700996d466
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2020-04-16 23:25:47 +0100

    Copy over html.ts as xml.ts

---
+++ src/xml.ts
@@ -0,0 +1,120 @@
+import {
+	Default,
+	Environment,
+	Intrinsic,
+	IntrinsicProps,
+	Portal,
+	Renderer,
+	Text,
+} from "@bikeshaving/crank/cjs/index";
+
+// declare module "./index" {
+// 	interface EventMap extends GlobalEventHandlersEventMap {}
+// }
+
+function escapeText(text: string): string {
+	return text.replace(/[&<>"']/g, (m) => {
+		switch (m) {
+			case "&":
+				return "&amp;";
+			case "<":
+				return "&lt;";
+			case ">":
+				return "&gt;";
+			case '"':
+				return "&quot;";
+			case "'":
+				return "&#039;";
+			default:
+				throw new Error("Bad match");
+		}
+	});
+}
+
+function printStyle(style: Record<string, string>): string {
+	const cssStrings = [];
+	for (const [name, value] of Object.entries(style)) {
+		if (value != null) {
+			cssStrings.push(`${name}:${value};`);
+		}
+	}
+
+	return cssStrings.join("");
+}
+
+function printAttrs(props: IntrinsicProps<string>): string {
+	const attrs: string[] = [];
+	for (const [name, value] of Object.entries(props)) {
+		switch (true) {
+			case name === "children":
+			case name === "innerHTML":
+				break;
+			case name === "style":
+				attrs.push(`style="${printStyle(value)}"`);
+				break;
+			case typeof value === "string":
+				attrs.push(`${escapeText(name)}="${escapeText(value)}"`);
+				break;
+			case typeof value === "number":
+				attrs.push(`${escapeText(name)}="${value}"`);
+				break;
+			case typeof value === "boolean":
+				attrs.push(`${escapeText(name)}`);
+				break;
+		}
+	}
+
+	return attrs.join(" ");
+}
+
+const voidTags = new Set([
+	"area",
+	"base",
+	"br",
+	"col",
+	"command",
+	"embed",
+	"hr",
+	"img",
+	"input",
+	"keygen",
+	"link",
+	"meta",
+	"param",
+	"source",
+	"track",
+	"wbr",
+]);
+
+export const env: Environment<string> = {
+	[Default](tag: string): Intrinsic<string> {
+		return function defaultString(props: IntrinsicProps<string>): string {
+			const attrs = printAttrs(props);
+			const open = `<${tag}${attrs.length ? " " : ""}${attrs}>`;
+			if (voidTags.has(tag)) {
+				return open;
+			}
+
+			const close = `</${tag}>`;
+			if ("innerHTML" in props) {
+				return `${open}${props["innerHTML"]}${close}`;
+			}
+
+			return `${open}${props.children.join("")}${close}`;
+		};
+	},
+	[Text](text: string): string {
+		return escapeText(text);
+	},
+	[Portal]({children}): string {
+		return children.join("");
+	},
+};
+
+export class StringRenderer extends Renderer<string> {
+	constructor() {
+		super(env);
+	}
+}
+
+export const renderer = new StringRenderer();
