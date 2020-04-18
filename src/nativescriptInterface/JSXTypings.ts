59e68846df1cbd408af4acd170baa764ecb716e8
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2020-04-18 14:18:46 +0100

    ReactNode -> Child

--- src/nativescriptInterface/JSXTypings.ts
+++ src/nativescriptInterface/JSXTypings.ts
@@ -1,35 +1,7 @@
-import { ActionBarProps, ActionItemProps, ActivityIndicatorProps, ButtonProps, ContentViewProps, DatePickerProps, FormattedStringProps, SpanProps, HtmlViewProps, ImageProps, LabelProps, AbsoluteLayoutProps, DockLayoutProps, FlexboxLayoutProps, GridLayoutProps, StackLayoutProps, WrapLayoutProps, ListPickerProps, ListViewProps, NavigationButtonProps, PlaceholderProps, ProgressProps, ScrollViewProps, SearchBarProps, SegmentedBarProps, SegmentedBarItemProps, SliderProps, SwitchProps, TabViewProps, TabViewItemProps, TextViewProps, TextFieldProps, TimePickerProps, WebViewProps, FrameProps, PageProps } from "./NativeScriptComponentTypings";
+import type { ActionBarProps, ActionItemProps, ActivityIndicatorProps, ButtonProps, ContentViewProps, DatePickerProps, FormattedStringProps, SpanProps, HtmlViewProps, ImageProps, LabelProps, AbsoluteLayoutProps, DockLayoutProps, FlexboxLayoutProps, GridLayoutProps, StackLayoutProps, WrapLayoutProps, ListPickerProps, ListViewProps, NavigationButtonProps, PlaceholderProps, ProgressProps, ScrollViewProps, SearchBarProps, SegmentedBarProps, SegmentedBarItemProps, SliderProps, SwitchProps, TabViewProps, TabViewItemProps, TextViewProps, TextFieldProps, TimePickerProps, WebViewProps, FrameProps, PageProps } from "./NativeScriptComponentTypings";
+import type { Child } from "@bikeshaving/crank/cjs";
 
-/*
- * Proof of concept for how we'd type ReactNode, though it seems that Crank nodes
- * are somewhat different.
- */
-
-/* Modified from React in that, in Crank, there is no ComponentLifecycle to extend. */
-interface Component<P = {}, S = {}, SS = any> { }
-
-type JSXElementConstructor<P> =
-| ((props: P) => ReactElement | null)
-| (new (props: P) => Component<P, any>);
-
-interface ReactElement<P = any, T extends string | JSXElementConstructor<any> = string | JSXElementConstructor<any>> {
-    type: T;
-    props: P;
-    key: Key | null;
-}
-type ReactText = string | number;
-type ReactChild = ReactElement | ReactText;
-
-interface ReactNodeArray extends Array<ReactNode> {}
-type ReactFragment = {} | ReactNodeArray;
-type Key = string | number;
-interface ReactPortal extends ReactElement {
-    key: Key | null;
-    children: ReactNode;
-}
-type ReactNode = ReactChild | ReactFragment | ReactPortal | boolean | null | undefined;
-
-type PropsWithChildren<P> = P & { children?: ReactNode };
+type PropsWithChildren<P> = P & { children?: Child };
 
 declare global {
 	module JSX {
