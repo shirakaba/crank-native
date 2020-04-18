a023536444d7a6ab9e2e07d45f00151e51556a65
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2020-04-18 14:20:57 +0100

    Move JSXTypings to index.ts to ensure import

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+import type { View } from "@nativescript/core";
+import type { Element, Child } from "@bikeshaving/crank/cjs";
+import type { ActionBarProps, ActionItemProps, ActivityIndicatorProps, ButtonProps, ContentViewProps, DatePickerProps, FormattedStringProps, SpanProps, HtmlViewProps, ImageProps, LabelProps, AbsoluteLayoutProps, DockLayoutProps, FlexboxLayoutProps, GridLayoutProps, StackLayoutProps, WrapLayoutProps, ListPickerProps, ListViewProps, NavigationButtonProps, PlaceholderProps, ProgressProps, ScrollViewProps, SearchBarProps, SegmentedBarProps, SegmentedBarItemProps, SliderProps, SwitchProps, TabViewProps, TabViewItemProps, TextViewProps, TextFieldProps, TimePickerProps, WebViewProps, FrameProps, PageProps } from "./nativescriptInterface/NativeScriptComponentTypings";
 import { renderer } from "./ns";
+import { Application } from "@nativescript/core";
 import * as console from "./Logger";
-import { Application, View } from "@nativescript/core";
-import { Element } from "@bikeshaving/crank/cjs";
 
 /**
  * Start up your Crank Native app, rendering a Crank component tree into a NativeScript host element.
@@ -29,4 +31,53 @@ export function start(App: Element, rootView: View): void {
             return rootView;
         },
     });
+}
+
+type PropsWithChildren<P> = P & { children?: Child };
+
+declare global {
+	module JSX {
+		interface IntrinsicElements {
+            actionBar: PropsWithChildren<ActionBarProps>,
+            actionItem: PropsWithChildren<ActionItemProps>,
+            activityIndicator: PropsWithChildren<ActivityIndicatorProps>,
+            button: PropsWithChildren<ButtonProps>,
+            contentView: PropsWithChildren<ContentViewProps>,
+            datePicker: PropsWithChildren<DatePickerProps>,
+            formattedString: PropsWithChildren<FormattedStringProps>,
+            span: PropsWithChildren<SpanProps>,
+            htmlView: PropsWithChildren<HtmlViewProps>,
+            image: PropsWithChildren<ImageProps>,
+            label: PropsWithChildren<LabelProps>,
+            absoluteLayout: PropsWithChildren<AbsoluteLayoutProps>,
+            dockLayout: PropsWithChildren<DockLayoutProps>,
+            flexboxLayout: PropsWithChildren<FlexboxLayoutProps>,
+            gridLayout: PropsWithChildren<GridLayoutProps>,
+            stackLayout: PropsWithChildren<StackLayoutProps>,
+            wrapLayout: PropsWithChildren<WrapLayoutProps>,
+            listPicker: PropsWithChildren<ListPickerProps>,
+            listView: PropsWithChildren<ListViewProps>,
+            navigationButton: PropsWithChildren<NavigationButtonProps>,
+            placeholder: PropsWithChildren<PlaceholderProps>,
+            progress: PropsWithChildren<ProgressProps>,
+            scrollView: PropsWithChildren<ScrollViewProps>,
+            searchBar: PropsWithChildren<SearchBarProps>,
+            segmentedBar: PropsWithChildren<SegmentedBarProps>,
+            segmentedBarItem: PropsWithChildren<SegmentedBarItemProps>,
+            slider: PropsWithChildren<SliderProps>,
+            switch: PropsWithChildren<SwitchProps>,
+            tabView: PropsWithChildren<TabViewProps>,
+            tabViewItem: PropsWithChildren<TabViewItemProps>,
+            textView: PropsWithChildren<TextViewProps>,
+            textField: PropsWithChildren<TextFieldProps>,
+            timePicker: PropsWithChildren<TimePickerProps>,
+            webView: PropsWithChildren<WebViewProps>,
+            frame: PropsWithChildren<FrameProps>,
+            page: PropsWithChildren<PageProps>,
+		}
+
+		interface ElementChildrenAttribute {
+			children: {};
+		}
+	}
 }
